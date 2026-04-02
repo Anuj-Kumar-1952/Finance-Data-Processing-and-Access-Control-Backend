4942b38df365bc1a56c141ab6ddee1d1f3950093
Anuj-Kumar-1952 <ak1952002@gmail.com>
2026-04-02 13:38:11 +0530

    Update security config with JWT filter and stateless setup

--- src/main/java/com/anuj/finance/backend/config/SecurityConfig.java
+++ src/main/java/com/anuj/finance/backend/config/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.anuj.finance.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import com.anuj.finance.backend.security.JwtFilter;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
