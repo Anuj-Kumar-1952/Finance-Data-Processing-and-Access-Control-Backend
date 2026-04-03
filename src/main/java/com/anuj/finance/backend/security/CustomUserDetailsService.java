f8e89314b95bd87b6ef5edcdfa57dcc6c5882777
Anuj-Kumar-1952 <ak1952002@gmail.com>
2026-04-03 14:29:25 +0530

    Improved security setup by refactoring SecurityConfig, updating JWT filter, and adding CustomUserDetailsService

---
+++ src/main/java/com/anuj/finance/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.anuj.finance.backend.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.anuj.finance.backend.entity.User;
+import com.anuj.finance.backend.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_" + user.getRole().name())
+                .accountLocked(!user.isActive()) // optional if you have active flag
+                .build();
+    }
+}
--- src/main/java/com/anuj/finance/backend/security/JwtFilter.java
+++ src/main/java/com/anuj/finance/backend/security/JwtFilter.java
@@ -1,20 +1,22 @@
 package com.anuj.finance.backend.security;
 
-import com.anuj.finance.backend.entity.User;
-import com.anuj.finance.backend.repository.UserRepository;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.List;
+import com.anuj.finance.backend.entity.User;
+import com.anuj.finance.backend.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
